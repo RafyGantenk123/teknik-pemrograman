ab149c93ec73ae2643a7c2c961d629943500a36e
RafyGantenk123 <rafy090607@gmail.com>
2026-03-06 21:57:37 +0700

    menambahkan source code no 3

---
+++ W4_Polymorphism/3-Polymorphic Sorting/Strings.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Strings
+{
+    public static void main(String[] args)
+    {
+        String[] strList;
+        int size;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many strings? ");
+        size = scan.nextInt();
+        scan.nextLine();
+
+        strList = new String[size];
+
+        System.out.println("Enter the strings:");
+
+        for(int i=0;i<size;i++)
+            strList[i] = scan.nextLine();
+
+        Sorting.selectionSort(strList);
+
+        System.out.println("Sorted strings:");
+
+        for(String s : strList)
+            System.out.println(s);
+    }
+}
